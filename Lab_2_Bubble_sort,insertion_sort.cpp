f0ec282d76d5e676c29909594cfd7fe2609a6c27
Vishal-S-Hayyal <104269176+Vishal-S-Hayyal@users.noreply.github.com>
2023-01-27 01:41:45 +0530

    Create Lab_2_Bubble_sort,insertion_sort.cpp

---
+++ Lab_2_Bubble_sort,insertion_sort.cpp
@@ -0,0 +1,79 @@
+/*pseudocode for insertion sort
+for(i=1;i<n;i++)
+{ temp =a[i];
+j=i-1;
+while(i>=0&&a[j]>temp)
+{ a[j+1]=a[j]
+  j--}
+  a [j+1]=temp;
+  }*/
+
+
+#include <iostream>
+using namespace std;
+
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void insertionSort(int array[], int size) {
+  for (int i= 1; i < size; step++) {
+    int key = array[i];
+    int j = i - 1;
+     while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  cout << "Sorted array in ascending order:\n";
+  printArray(data, size);
+}
+
+/*pseudocode for bubble sort
+Bubble Sort(arr, size)
+		for (i = 0; i < n - 1; i++)
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1])*/
+/*bubble sort sample program in c++*/
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	for (i = 0; i < n - 1; i++)
+		for (j = 0; j < n - i - 1; j++)
+			if (arr[j] > arr[j + 1])
+				swap(arr[j], arr[j + 1]);
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	int arr[] = { 23,10,16,11,20};
+	int N = sizeof(arr) / sizeof(arr[0]);
+	bubbleSort(arr, N);
+	cout << "Sorted array: \n";
+	printArray(arr, N);
+	return 0;
+}
+
